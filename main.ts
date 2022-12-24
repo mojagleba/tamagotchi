2ecd339a2a26e1cbd9751caa8bfd80249677b9d9
mojagleba <121286172+mojagleba@users.noreply.github.com>
2022-12-24 02:33:50 +0100

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,230 @@
+input.onButtonPressed(Button.A, function () {
+    Kosc = 1
+    Akcja = 1
+})
+input.onButtonPressed(Button.B, function () {
+    Spacer = 1
+    Akcja = 1
+})
+let Akcja = 0
+let Kosc = 0
+let Spacer = 0
+let Zycie = 1
+let Szczescie = 10
+Spacer = 0
+Kosc = 0
+Akcja = 0
+loops.everyInterval(6000, function () {
+    Szczescie += -1
+})
+loops.everyInterval(60000, function () {
+    Spacer += -1
+    Akcja = 1
+})
+basic.forever(function () {
+    while (Zycie == 1) {
+        if (Akcja == 1) {
+            if (Szczescie < 10 && Kosc == 1) {
+                basic.showLeds(`
+                    . # . . #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    . . # # .
+                    . # . . #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    . # . . #
+                    . . # # .
+                    . # . . #
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . # . . #
+                    . . # # .
+                    . # . . #
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . # . . #
+                    . . # # .
+                    . # . . #
+                    `)
+                Szczescie += 1
+                basic.showIcon(IconNames.Heart)
+                basic.clearScreen()
+                basic.pause(500)
+                basic.showIcon(IconNames.Heart)
+                basic.clearScreen()
+                basic.pause(500)
+                Kosc = 0
+                Akcja = 0
+            }
+            if (Spacer > 0) {
+                basic.showLeds(`
+                    . # . . .
+                    # # . # .
+                    # # . # .
+                    . # # # .
+                    . # . # .
+                    `)
+                basic.showLeds(`
+                    # . . . .
+                    # . # . .
+                    # . # # .
+                    # # # . .
+                    # . # . .
+                    `)
+                basic.showLeds(`
+                    # . . . .
+                    # . # . #
+                    # . # . .
+                    # # # . .
+                    # . # . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . # . . .
+                    . # . . #
+                    # # . . .
+                    . # . . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    # . . . .
+                    # . . . .
+                    # . . . #
+                    # . . . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . #
+                    `)
+                Spacer = 0
+                Akcja = 0
+            }
+            if (Spacer < 0) {
+                basic.showLeds(`
+                    . # . . .
+                    # # . . .
+                    # # . . .
+                    . # # # .
+                    . # . # .
+                    `)
+                basic.showLeds(`
+                    . # . . .
+                    # # . . .
+                    # # . . .
+                    . # # . .
+                    . # . # .
+                    `)
+                basic.showLeds(`
+                    . # . . .
+                    # # . . .
+                    # # . . .
+                    . # # # .
+                    . # . # #
+                    `)
+                basic.showLeds(`
+                    # . . . .
+                    # . . . .
+                    # . . . .
+                    # # # . #
+                    # . # # #
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # # . # .
+                    . # # # #
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . # # # .
+                    `)
+                Spacer = 0
+                Akcja = 0
+            }
+        } else {
+            if (Szczescie == 10 || Szczescie > 10) {
+                basic.showLeds(`
+                    . # . . #
+                    # # . # .
+                    # # . . #
+                    . # # # .
+                    . # . # .
+                    `)
+                basic.showLeds(`
+                    . # . # .
+                    # # . . #
+                    # # . # .
+                    . # # # .
+                    . # . # .
+                    `)
+            }
+            if (Szczescie < 10 && Szczescie >= 5) {
+                basic.showLeds(`
+                    . # . . .
+                    # # . # .
+                    # # . . #
+                    . # # # .
+                    . # . # .
+                    `)
+                basic.showLeds(`
+                    . # . . .
+                    # # . . #
+                    # # . # .
+                    . # # # .
+                    . # . # .
+                    `)
+            }
+            if (Szczescie < 5 && Szczescie >= 1) {
+                basic.showLeds(`
+                    . # . . .
+                    # # . . .
+                    # # . . #
+                    . # # # .
+                    . # . # .
+                    `)
+                basic.showLeds(`
+                    . # . . .
+                    # # . . .
+                    # # . # .
+                    . # # # .
+                    . # . # .
+                    `)
+            }
+            if (Szczescie < 0 || Szczescie == 0) {
+                basic.showIcon(IconNames.No)
+                basic.showIcon(IconNames.No)
+                basic.showIcon(IconNames.Skull)
+                basic.showIcon(IconNames.Skull)
+                basic.showLeds(`
+                    . # . . .
+                    # # # . .
+                    . # . . .
+                    . # . # .
+                    . # # # #
+                    `)
+                Zycie = 0
+            }
+        }
+    }
+})
